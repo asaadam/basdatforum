0df676494e303447f54d7cf6d71a94e9d55f6c29
Asaadam <graha.asa@gmail.com>
2018-11-25 23:17:04 +0700

    Fix Auth

--- backend/auth/middleware.js
+++ backend/auth/middleware.js
@@ -2,19 +2,24 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
+
+
+//please be carefull , dont pass your token with "" , 
+//otherwise it will make you unathorized.
 function checkToken(req,res,next){
     console.log('check auth')
     const authHeader = req.get('authorization');
-        jwt.verify(authHeader,'asdfgh'),(err,user)=>{
+        jwt.verify(authHeader,process.env.TOKEN_SECRET,(err,user)=>{
             if (err){
-                console.log(err);
+                res.status(401).json("Unauthorized");
             }
-            else {
+            else{
                 req.user=user;
                 next();
             }
-        };
+        })
     }
+
   
 
 
